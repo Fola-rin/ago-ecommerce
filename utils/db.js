2be64d343eb02db46e8429697f78c23411f8440f
AfolarinOyelekeRumbl <oyelekefolarin@gmail.com>
2024-05-11 13:07:15 +0100

    use data.js file for api

--- utils/db.js
+++ utils/db.js
@@ -5,45 +5,50 @@ const connection = {};
 const connect = async () => {
 	let timeoutHandle;
 	try {
-		if (connection.isConnected === 1) {
-			console.log("connected already", connection.isConnected);
-			return true;
-		}
-		if (mongoose.connections.length > 0) {
-			connection.isConnected = mongoose.connections[0].readyState;
+		if (process.env.NODE_ENV === "development") {
 			if (connection.isConnected === 1) {
-				console.log("use previous connnection", connection.isConnected);
+				console.log("connected already", connection.isConnected);
 				return true;
 			}
-			await disconnect();
+			if (mongoose.connections.length > 0) {
+				connection.isConnected = mongoose.connections[0].readyState;
+				if (connection.isConnected === 1) {
+					console.log("use previous connnection", connection.isConnected);
+					return true;
+				}
+				await disconnect();
+			}
+			mongoose.set("strictQuery", true);
+			const dbPromise = mongoose.connect(process.env.MONGODB_URI);
+
+			// to handle cases where mongo db takes to long to respond, most likely due to errors
+			const timeoutPromise = new Promise((resolve) => {
+				timeoutHandle = setTimeout(
+					() => {
+						resolve(null);
+					}, 1000);
+			});
+
+			return Promise.race([dbPromise, timeoutPromise]).then((db) => {
+				if (db === null) {
+					console.log("Connection timed out!");
+					return false;
+				}
+				if (db.connections[0].readyState === 1) {
+					connection.isConnected = db.connections[0].readyState;
+					return true;
+				}
+					return false;
+				}
+			)
+			.catch((error) => {
+				console.log('ERROR-> mongoose stopped working', error);
+				return false;
+			});
 		}
-		mongoose.set("strictQuery", true);
-		const dbPromise = mongoose.connect(process.env.MONGODB_URI);
-
-		// to handle cases where mongo db takes to long to respond, most likely due to errors
-		const timeoutPromise = new Promise((resolve) => {
-			timeoutHandle = setTimeout(
-				() => {
-					resolve(null);
-				}, 1000);
-		});
-
-		return Promise.race([dbPromise, timeoutPromise]).then((db) => {
-			if (db === null) {
-				console.log("Connection timed out!");
-				return false;
-			}
-			if (db.connections[0].readyState === 1) {
-				connection.isConnected = db.connections[0].readyState;
-				return true;
-			}
-				return false;
-			}
-		)
-		.catch((error) => {
-			console.log('ERROR-> mongoose stopped working', error);
+		else {
 			return false;
-		});
+		}
 		
 	} catch (error) {
 		console.log('ERROR->', error?.reason?.setName)
